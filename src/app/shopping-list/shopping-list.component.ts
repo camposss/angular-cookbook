136548d61bfa0b0c1d8ae1d194ddc9500b27fc68
camposss <ccamposs23@gmail.com>
2018-10-29 06:54:05 -0700

    Refactored event emitters to use subjects/observables

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredients.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
@@ -9,13 +10,19 @@ import { ShoppingListService } from './shopping-list.service';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[];
+  private subscriptions: Subscription[] = [];
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged.subscribe( (ingredients: Ingredient[])=>{
+    this.subscriptions.push(this.slService.ingredientsChanged.subscribe( (ingredients: Ingredient[])=>{
       this.ingredients = ingredients;
-    })
+    }))
+  }
+  ngOnDestroy(){
+    this.subscriptions.forEach(
+      sub=> sub.unsubscribe()
+    )
   }
 }
